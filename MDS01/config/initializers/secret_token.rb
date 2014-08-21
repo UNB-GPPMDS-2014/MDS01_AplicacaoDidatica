dda0fa204e0c439c8cdcd05d2b187c230546ef81
Matheus S. Pereira <matheussilva.fga@gmail.com>
2014-08-21 12:45:56 -0300

    Criando projeto

---
+++ MDS01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MDS01::Application.config.secret_token = 'a4c86a7222517eda062bc6621ae49dc930743409857e6625758769bb38af0cadbce5abc048f2478ed130fbbe60a701fe7702f103f2867a30df559227ef6dfbd7'
